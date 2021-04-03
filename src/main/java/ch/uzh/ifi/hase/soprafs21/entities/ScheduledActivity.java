0de060ffa5e1a2d9c1ed0281ea418b0411cb4683
Samuel Brügger <SamuelBruegger@users.noreply.github.com>
2021-04-03 21:55:10 +0200

    created ScheduledActivity class

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entities/ScheduledActivity.java
@@ -0,0 +1,59 @@
+package ch.uzh.ifi.hase.soprafs21.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "ScheduledActivity")
+public class ScheduledActivity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private UserActivity chosenActivity;
+
+    @Column
+    private String chosenLocation;
+
+    @Column
+    private Date chosenDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserActivity getChosenActivity() {
+        return chosenActivity;
+    }
+
+    public void setChosenActivity(UserActivity chosenLocation) {
+        this.chosenActivity = chosenActivity;
+    }
+
+    public String getChosenLocation() {
+        return chosenLocation;
+    }
+
+    public void setChosenLocation(String chosenLocation) {
+        this.chosenLocation = chosenLocation;
+    }
+
+    public Date getChosenDate() {
+        return chosenDate;
+    }
+
+    public void setChosenDate(Date chosenDate) {
+        this.chosenDate = chosenDate;
+    }
+
+}
+

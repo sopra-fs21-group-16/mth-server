12a18fb9f9378cebca88039f09e443319765c4fc
AleksandarRistic99 <71384880+AleksandarRistic99@users.noreply.github.com>
2021-04-05 09:57:09 +0200

    Update comment

--- src/main/java/ch/uzh/ifi/hase/soprafs21/constant/SwipeStatus.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/constant/SwipeStatus.java
@@ -1,7 +1,7 @@
 package ch.uzh.ifi.hase.soprafs21.constant;
 
 /**
- * enum, such that DecisionStatus is extendable for the future (i.e. for functions like SUPERLIKE or BLACKLIST)
+ * enum, such that SwipeStatus is extendable for the future (i.e. for functions like SUPERLIKE or BLACKLIST)
  */
 public enum SwipeStatus {
     TRUE,FALSE;

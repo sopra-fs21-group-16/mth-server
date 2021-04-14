8c5fbba6bf3249a8854eebbd0c55dbe135ab75e2
Aleksandar Ristic <71384880+AleksandarRistic99@users.noreply.github.com>
2021-04-14 22:41:12 +0200

    Update ActivityPreset.java

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entities/ActivityPreset.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entities/ActivityPreset.java
@@ -22,7 +22,6 @@ public class ActivityPreset implements Serializable{
     @Column(nullable = false, unique = true)
     private String activityName;
 
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ActivityCategory activityCategory;

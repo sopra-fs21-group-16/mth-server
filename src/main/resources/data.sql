2ded41e3aca7e4736604beb6c032b6a90816f5ce
Tarek <tarekalakmeh@hotmail.ch>
2021-04-25 11:45:39 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,8 @@ INSERT INTO ACTIVITY_PRESET (ID, ACTIVITY_NAME, ACTIVITY_CATEGORY,GOOGLEPOICATEG
     (4, 'See a play','THEATRE','???','???'),
     (5, 'Eat sushi','EATING','???','???'),
     (6, 'Explore the city','SIGHTSEEING','???','???'),
-    (7, 'Make a pizza','MAKE_FOOD','???','???'),
+    (7, 'Make a pizza','COOKING','???','???'),
     (8, 'Go on a bike tour','OUTDOOR_ACTIVITY','???','???'),
     (9, 'Watch a horror movie','MOVIES','???','???'),
-    (10, 'Hiking on the Uetliberg','MOVIES','???','???')
+    (10, 'Hiking on the Uetliberg','SPORTS','???','???')
     ;

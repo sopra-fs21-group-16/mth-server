bb47eabc826251fd197b0a781e39e371509a526c
AleksandarRistic99 <71384880+AleksandarRistic99@users.noreply.github.com>
2021-04-25 13:10:28 +0200

    Update User.java

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entities/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entities/User.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 

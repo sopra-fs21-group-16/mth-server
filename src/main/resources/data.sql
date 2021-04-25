f9bef4c3e49b643bce6685620c3c0367d022467f
Tarek <tarekalakmeh@hotmail.ch>
2021-04-25 11:27:47 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO ACTIVITY_PRESET (ID, ACTIVITY_NAME, ACTIVITY_CATEGORY,GOOGLEPOICATEGORY, GOOGLEPOIKEYWORD) VALUES
-    (1, 'Play Football','SPORT','???','???'),
-    (2, 'Play Tennis','SPORT','???','???'),
+    (1, 'Play Football','SPORTS','???','???'),
+    (2, 'Play Tennis','SPORTS','???','???'),
     (3, 'Listen Concert at Tonhalle Zurich','MUSIC','???','???'),
     (4, 'See a play','THEATRE','???','???'),
-    (5, 'Eat sushi','FOOD','???','???'),
+    (5, 'Eat sushi','EATING','???','???'),
     (6, 'Explore the city','SIGHTSEEING','???','???'),
     (7, 'Make a pizza','MAKE_FOOD','???','???'),
     (8, 'Go on a bike tour','OUTDOOR_ACTIVITY','???','???'),

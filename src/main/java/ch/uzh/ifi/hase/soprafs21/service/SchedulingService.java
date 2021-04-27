225fb9a1165d30c5d4320d542c7222c003fc7288
Samuel Brügger <SamuelBruegger@users.noreply.github.com>
2021-04-27 12:46:20 +0200

    - added checks for token and additional constraints - createSchedulingSession returns a schedulingSession with a list with the activities where both user swiped right

--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/SchedulingService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/SchedulingService.java
@@ -1,9 +1,11 @@
 package ch.uzh.ifi.hase.soprafs21.service;
 
-import ch.uzh.ifi.hase.soprafs21.entities.ScheduledActivity;
-import ch.uzh.ifi.hase.soprafs21.entities.SchedulingSession;
+import ch.uzh.ifi.hase.soprafs21.constant.SwipeStatus;
+import ch.uzh.ifi.hase.soprafs21.entities.*;
+import ch.uzh.ifi.hase.soprafs21.repository.ActivityRepository;
 import ch.uzh.ifi.hase.soprafs21.repository.ScheduledActivityRepository;
 import ch.uzh.ifi.hase.soprafs21.repository.SchedulingSessionRepository;
+import ch.uzh.ifi.hase.soprafs21.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.ConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -29,22 +33,93 @@ public class SchedulingService {
 
     private final ScheduledActivityRepository scheduledActivityRepository;
 
+    private final ActivityRepository activityRepository;
+
+    private final UserRepository userRepository;
+
     @Autowired
-    public SchedulingService(@Qualifier("schedulingSessionRepository") SchedulingSessionRepository schedulingSessionRepository, @Qualifier("scheduledActivityRepository") ScheduledActivityRepository scheduledActivityRepository) {
+    public SchedulingService(@Qualifier("schedulingSessionRepository") SchedulingSessionRepository schedulingSessionRepository,
+                             @Qualifier("scheduledActivityRepository") ScheduledActivityRepository scheduledActivityRepository,
+                             @Qualifier("activityRepository") ActivityRepository activityRepository,
+                             @Qualifier("userRepository") UserRepository userRepository) {
         this.schedulingSessionRepository = schedulingSessionRepository;
         this.scheduledActivityRepository = scheduledActivityRepository;
+        this.activityRepository = activityRepository;
+        this.userRepository = userRepository;
+    }
+
+    public SchedulingSession createSchedulingSession(Long userId1, Long userId2, String token) {
+        SchedulingSession schedulingSession = new SchedulingSession();
+        User user = userRepository.findByToken(token);
+        if (!user.getId().equals(userId1) && !user.getId().equals(userId2)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User can not start scheduling Session of other users.");
+        }
+
+        List<Activity> activityList = activityRepository.findAll();
+        List<Activity> sessionActivityList = new ArrayList<Activity>();
+        for (Activity activity : activityList) {
+            boolean userId1Found = false;
+            boolean userId2Found = false;
+            for (UserSwipeStatus userSwipeStatus : activity.getUserSwipeStatusList()) {
+                if (userSwipeStatus.getUser().getId().equals(userId1) && userSwipeStatus.getSwipeStatus().equals(SwipeStatus.TRUE)) {
+                    userId1Found = true;
+                }
+                if (userSwipeStatus.getUser().getId().equals(userId2) && userSwipeStatus.getSwipeStatus().equals(SwipeStatus.TRUE)) {
+                    userId2Found = true;
+                }
+            }
+            if (userId1Found && userId2Found) {
+                sessionActivityList.add(activity);
+            }
+        }
+        if (sessionActivityList.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Scheduling session not possible for these two users. No common activities.");
+        }
+        schedulingSession.setActivityList(sessionActivityList);
+        SchedulingSession newSchedulingSession = schedulingSessionRepository.save(schedulingSession);
+        schedulingSessionRepository.flush();
+        return newSchedulingSession;
     }
 
     public ScheduledActivity saveScheduledActivity(long sessionId, ScheduledActivity scheduledActivity) {
+        SchedulingSession schedulingSession = schedulingSessionRepository.findById(sessionId);
+        if (schedulingSession == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Scheduling session not found");
+        }
+        if (scheduledActivity.getActivity() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Scheduled activity misses specific activity.");
+        }
+        if (scheduledActivity.getDate() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Scheduled activity misses specific date.");
+        }
+        if (scheduledActivity.getLocation() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Scheduled activity misses specific location.");
+        }
+
         ScheduledActivity newScheduledActivity = scheduledActivityRepository.save(scheduledActivity);
         scheduledActivityRepository.flush();
-        schedulingSessionRepository.delete(schedulingSessionRepository.findById(sessionId));
+        schedulingSessionRepository.delete(schedulingSession);
         return newScheduledActivity;
     }
 
     public SchedulingSession getSchedulingSession(long sessionId, String token) {
+        SchedulingSession schedulingSession = schedulingSessionRepository.findById(sessionId);
+        if (schedulingSession == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Scheduling session Not Found");
+        }
+        boolean isPartOfMatch = false;
+        List<UserSwipeStatus> userSwipeStatusList = schedulingSession.getActivityList().get(0).getUserSwipeStatusList();
 
-        return schedulingSessionRepository.findById(sessionId);
+        for (UserSwipeStatus userSwipeStatus : userSwipeStatusList) {
+            if (userSwipeStatus.getUser().getToken().equals(token)) {
+                isPartOfMatch = true;
+                break;
+            }
+        }
+        if (!isPartOfMatch) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User is not part of the scheduling session");
+        }
+        return schedulingSession;
     }
 
     public void updateSchedulingSession(long sessionId, SchedulingSession schedulingSession, String token) {
@@ -127,12 +202,5 @@ public class SchedulingService {
         });
 
         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, exceptions.toString());
-    }
-
-    public SchedulingSession createSchedulingSession(Long userId1, Long userId2, String token) {
-        SchedulingSession schedulingSession = new SchedulingSession();
-        SchedulingSession newSchedulingSession = schedulingSessionRepository.save(schedulingSession);
-        schedulingSessionRepository.flush();
-        return newSchedulingSession;
     }
 }

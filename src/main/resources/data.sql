4e94bd19af71f28e6cac8f33bb68a475286f8000
AleksandarRistic99 <71384880+AleksandarRistic99@users.noreply.github.com>
2021-04-28 15:01:32 +0200

    update SQL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,12 +14,12 @@ VALUES (1, 'Play Football', 'SPORTS', '???', '???'),
 
 INSERT INTO USER (ID, EMAIL, PASSWORD, BIO, PHONE, TOKEN, NAME, DATE_OF_BIRTH, GENDER, PROFILE_PICTURE, LAST_SEEN,
                   USER_INTERESTS_ID)
-VALUES (10, 'database.user@uzh.ch', 'databasePassword', 'databaseBio', '+41791231111', 'databaseToken', 'databaseTester', NULL, 'MALE', 'TestLink', '2021-04-25', NULL),
-       (20, 'database.user2@uzh.ch', 'databasePassword2', 'databaseBio2', '+41791232222', 'databaseToken2','databaseTester2', NULL, 'FEMALE', 'TestLink2', '2021-04-26', NULL);
+VALUES (1, 'database.user@uzh.ch', 'databasePassword', 'databaseBio', '+41791231111', 'databaseToken', 'databaseTester', NULL, 'MALE', 'TestLink', '2021-04-25', NULL),
+       (2, 'database.user2@uzh.ch', 'databasePassword2', 'databaseBio2', '+41791232222', 'databaseToken2','databaseTester2', NULL, 'FEMALE', 'TestLink2', '2021-04-26', NULL);
 
 INSERT INTO USER_INTERESTS (ID, AGE_RANGE_MIN, AGE_RANGE_MAX, GENDER_PREFERENCE, USER_ID)
-VALUES (1, 18, 30, 'FEMALE', 10),
-       (2, 21, 26, 'MALE', 20);
+VALUES (1, 18, 30, 'FEMALE', 1),
+       (2, 21, 26, 'MALE', 2);
 
 INSERT INTO USER_INTERESTS_ACTIVITY (USER_INTERESTS_ID, ACTIVITY_INTERESTS)
 VALUES (1, 'EATING'),
@@ -36,8 +36,8 @@ VALUES (1, 'EATING'),
 
 UPDATE USER
 SET USER_INTERESTS_ID=1
-WHERE ID = 10;
+WHERE ID = 1;
 
 UPDATE USER
 SET USER_INTERESTS_ID=2
-WHERE ID = 20;
+WHERE ID = 2;

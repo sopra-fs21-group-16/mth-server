757268403677e4b1a7255c000dc018ba245f10ac
Tarek <tarekalakmeh@hotmail.ch>
2021-04-28 02:33:39 +0200

    Add ActivityCategory Endpoint

--- src/main/java/ch/uzh/ifi/hase/soprafs21/constant/ActivityCategory.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/constant/ActivityCategory.java
@@ -1,18 +1,28 @@
 package ch.uzh.ifi.hase.soprafs21.constant;
 
 public enum ActivityCategory {
-    COOKING,
-    EATING,
-    THEATRE,
-    MOVIES,
-    MUSIC,
-    MUSEUMS,
-    OUTDOOR_ACTIVITY,
-    SIGHTSEEING,
-    TRAVEL,
-    WELLNESS,
-    SHOPPING,
-    GAMES,
-    SPORTS,
-    WINTERSPORTS
+    COOKING("cooking-pot"),
+    EATING("hamburger"),
+    THEATRE("comedy"),
+    MOVIES("movie-projector"),
+    MUSIC("music"),
+    MUSEUMS("bust"),
+    OUTDOOR_ACTIVITY("campfire"),
+    SIGHTSEEING("camera"),
+    TRAVEL("airport"),
+    WELLNESS("spa-flower"),
+    SHOPPING("shopping-bag"),
+    GAMES("game-controller"),
+    SPORTS("track-and-field"),
+    WINTERSPORTS("skiing");
+
+    public final String icon;
+
+    ActivityCategory(String icon) {
+        this.icon = icon;
+    }
+
+    public String toString() {
+        return this.icon;
+    }
 }

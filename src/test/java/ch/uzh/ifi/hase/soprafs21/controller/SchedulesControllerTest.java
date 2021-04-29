4dfcdf0311bac4f6b6f915f367790d7b835b1e10
AleksandarRistic99 <71384880+AleksandarRistic99@users.noreply.github.com>
2021-04-29 16:44:22 +0200

    Create SchedulesControllerTest.java

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/controller/SchedulesControllerTest.java
@@ -0,0 +1,28 @@
+package ch.uzh.ifi.hase.soprafs21.controller;
+
+import ch.uzh.ifi.hase.soprafs21.service.SchedulingService;
+import ch.uzh.ifi.hase.soprafs21.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(SchedulesController.class)
+class SchedulesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SchedulingService schedulingService;
+
+    @MockBean
+    private UserService userService;
+
+   
+
+
+}

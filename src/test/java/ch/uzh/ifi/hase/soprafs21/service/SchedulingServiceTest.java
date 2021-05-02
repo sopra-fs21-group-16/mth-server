e316051dc92401f9d9a42d0f13e1f0a792102a74
Tarek <tarekalakmeh@hotmail.ch>
2021-05-02 11:15:53 +0200

    Update SchedulingServiceTest.java

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/SchedulingServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/SchedulingServiceTest.java
@@ -24,5 +24,90 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchedulingServiceTest {
+    @Mock
+    private ScheduledActivityRepository scheduledActivityRepository;
+
+    @Mock
+    private SchedulingSessionRepository schedulingSessionRepository;
+
+    @Mock
+    private ActivityRepository activityRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private SchedulingService schedulingService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void deleteScheduledSession_success() {
+        //given
+        User testUser1 = new User();
+        testUser1.setId(1L);
+        testUser1.setToken("Token");
+        User testUser2 = new User();
+        testUser2.setId(2L);
+        testUser2.setToken("Token2");
+        Activity testActivity = new Activity();
+        testActivity.setId(11L);
+        List<UserSwipeStatus> userSwipeStatusList = new ArrayList<>();
+        UserSwipeStatus userSwipeStatus1 = new UserSwipeStatus();
+        UserSwipeStatus userSwipeStatus2 = new UserSwipeStatus();
+        userSwipeStatus1.setUser(testUser1);
+        userSwipeStatus1.setSwipeStatus(SwipeStatus.TRUE);
+        userSwipeStatus2.setUser(testUser2);
+        userSwipeStatus2.setSwipeStatus(SwipeStatus.TRUE);
+        userSwipeStatusList.add(userSwipeStatus1);
+        userSwipeStatusList.add(userSwipeStatus2);
+        testActivity.setUserSwipeStatusList(userSwipeStatusList);
+        List<Activity> activityList = new ArrayList<>();
+        activityList.add(testActivity);
+        SchedulingSession schedulingSession = new SchedulingSession();
+        schedulingSession.setId(1L);
+        schedulingSession.setActivityList(activityList);
+
+        //when
+        schedulingService.deleteScheduledSession(schedulingSession.getId());
+
+        Mockito.verify(schedulingSessionRepository, Mockito.times(1)).delete(Mockito.any());
+    }
+
+    @Test
+    void checkIfScheduledSessionExistsWithGivenId_InvalidId_throwException() {
+        //given
+        User testUser1 = new User();
+        testUser1.setId(1L);
+        testUser1.setToken("Token");
+        User testUser2 = new User();
+        testUser2.setId(2L);
+        testUser2.setToken("Token2");
+        Activity testActivity = new Activity();
+        testActivity.setId(11L);
+        List<UserSwipeStatus> userSwipeStatusList = new ArrayList<>();
+        UserSwipeStatus userSwipeStatus1 = new UserSwipeStatus();
+        UserSwipeStatus userSwipeStatus2 = new UserSwipeStatus();
+        userSwipeStatus1.setUser(testUser1);
+        userSwipeStatus1.setSwipeStatus(SwipeStatus.TRUE);
+        userSwipeStatus2.setUser(testUser2);
+        userSwipeStatus2.setSwipeStatus(SwipeStatus.TRUE);
+        userSwipeStatusList.add(userSwipeStatus1);
+        userSwipeStatusList.add(userSwipeStatus2);
+        testActivity.setUserSwipeStatusList(userSwipeStatusList);
+        List<Activity> activityList = new ArrayList<>();
+        activityList.add(testActivity);
+        SchedulingSession schedulingSession = new SchedulingSession();
+        schedulingSession.setId(1L);
+        schedulingSession.setActivityList(activityList);
+
+        //when
+        doThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Scheduling session with session id " + schedulingSession.getId() + " was not found"))).when(schedulingSessionRepository).findById(Mockito.anyLong());
+
+        assertThrows(ResponseStatusException.class, () -> schedulingService.checkIfScheduledSessionExistsWithGivenId(schedulingSession.getId()));
+    }
 
 }

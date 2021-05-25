af2a9a80763e457168c4b71802ecd3c3572b187b
AleksandarRistic99 <71384880+AleksandarRistic99@users.noreply.github.com>
2021-05-25 15:34:24 +0200

    Update UserServiceIntegrationTest.java

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/UserServiceIntegrationTest.java
@@ -231,6 +231,7 @@ public class UserServiceIntegrationTest {
         userRepository.delete(createdUserWithID);
     }
 
+    @Test
     public void checkIfValidToken_WrongToken_NoSuccess(){
         assertNull(userRepository.findByEmail("test.user@uzh.ch"));
 
